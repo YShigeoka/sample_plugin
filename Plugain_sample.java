3470f252316358e5ff9aba826ef0d55a38320f6c
YShigeoka <ysnr.na.chan@gmail.com>
2023-09-14 20:08:02 +0900

    sample_plugin

---
+++ Plugain_sample.java
@@ -0,0 +1,17 @@
+package plugin.plugain_sample;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Plugain_sample extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
